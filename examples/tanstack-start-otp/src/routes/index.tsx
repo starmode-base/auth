8835052202dd92532ea8502a10280e8b043b1cbe
Mikael Lirbank <mikael@lirbank.com>
2026-02-20 05:46:27 -0800

    Examples

--- examples/tanstack-start-otp/src/routes/index.tsx
+++ examples/tanstack-start-otp/src/routes/index.tsx
@@ -4,33 +4,102 @@ import { useState } from "react";
 
 export const Route = createFileRoute("/")({ component: App });
 
+type Step = "request-otp" | "verify-otp";
+
+function RequestOtpStep(props: { onContinue: (email: string) => void }) {
+  const [email, setEmail] = useState("");
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 text-center">
+        <h1 className="text-3xl font-semibold">Welcome!</h1>
+        <div className="text-gray-500">Let's get you signed in.</div>
+      </div>
+      <input
+        type="email"
+        placeholder="Email address"
+        className="h-10 border-b border-gray-300 bg-transparent placeholder:text-gray-500"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-full bg-gray-900 py-3 text-white hover:bg-gray-800"
+        onClick={() => {
+          props.onContinue(email);
+        }}
+      >
+        Send one-time password
+      </button>
+    </>
+  );
+}
+
+function VerifyOtpStep(props: { onContinue: (otp: string) => void }) {
+  const [otp, setOtp] = useState("");
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 text-center">
+        <h1 className="text-3xl font-semibold">Check your email</h1>
+        <div className="text-gray-500">Enter your one-time password.</div>
+      </div>
+      <input
+        type="text"
+        placeholder="One-time password"
+        className="h-10 border-b border-gray-300 bg-transparent placeholder:text-gray-500"
+        value={otp}
+        onChange={(e) => setOtp(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-full bg-gray-900 py-3 text-white hover:bg-gray-800"
+        onClick={() => {
+          props.onContinue(otp);
+        }}
+      >
+        Continue
+      </button>
+    </>
+  );
+}
+
 function App() {
+  const [step, setStep] = useState<Step>("request-otp");
   const [email, setEmail] = useState("");
 
   return (
     <div className="grid min-h-dvh grid-cols-2 gap-4 p-4">
       <div className="m-auto flex w-full max-w-sm flex-col gap-8 p-8">
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-3xl font-semibold">Welcome!</h1>
-          <div className="text-gray-500">Let's get you signed in.</div>
-        </div>
-        <input
-          type="email"
-          placeholder="Email"
-          className="h-10 border-b border-gray-300 bg-transparent placeholder:text-gray-500"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="rounded-full bg-gray-900 py-3 text-white hover:bg-gray-800"
-          onClick={async () => {
-            const result = await authClient.requestOtp({ identifier: email });
-            console.log("result", result);
-          }}
-        >
-          Continue with email
-        </button>
+        {step === "request-otp" ? (
+          <RequestOtpStep
+            onContinue={async (email) => {
+              const result = await authClient.requestOtp({ identifier: email });
+              if (result.success) {
+                setStep("verify-otp");
+                setEmail(email);
+              } else {
+                // setError("Failed to send OTP");
+              }
+            }}
+          />
+        ) : step === "verify-otp" ? (
+          <VerifyOtpStep
+            onContinue={async (otp) => {
+              const result = await authClient.verifyOtp({
+                identifier: email,
+                otp,
+              });
+
+              console.log("result", result);
+              if (result.success) {
+                setStep("request-otp");
+              } else {
+                // setError("Invalid OTP");
+              }
+            }}
+          />
+        ) : null}
       </div>
       <div className="flex gap-8 rounded-xl bg-pink-500 p-8">
         <div className="m-auto text-center">
