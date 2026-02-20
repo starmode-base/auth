a8e001efca2ea01069c281a8b73cee8198ccfebc
Mikael Lirbank <mikael@lirbank.com>
2026-02-19 20:45:05 -0800

    Examples

--- examples/tanstack-start/vite.config.ts
+++ examples/tanstack-start/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [
-    nitro(),
-    viteTsConfigPaths(),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+  plugins: [tailwindcss(), tanstackStart(), viteReact()],
 });
