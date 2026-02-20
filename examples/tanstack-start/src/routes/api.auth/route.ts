4a54a4585ae5cd7e3272f12ab3b0ab31ced23698
Mikael Lirbank <mikael@lirbank.com>
2026-02-19 21:01:07 -0800

    Examples

--- examples/tanstack-start/src/routes/api.auth/route.ts
+++ examples/tanstack-start/src/routes/api.auth/route.ts
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { makeAuthHandler } from "@starmode/auth";
-import { auth } from "~/lib/auth";
+import { auth } from "../../lib/auth";
 
 export const Route = createFileRoute("/api/auth")({
   server: { handlers: makeAuthHandler(auth) },
