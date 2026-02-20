cf05d164be381ff38023ed793565d0926463b050
Mikael Lirbank <mikael@lirbank.com>
2026-02-19 19:52:53 -0800

    Scaffold tanstack-start-otp

---
+++ examples/tanstack-start-otp/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+const config = defineConfig({
+  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+});
+
+export default config;
