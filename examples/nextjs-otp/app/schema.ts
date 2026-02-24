ba8035d70cd0f5f19920f4b7802c37ebc490edc0
Mikael Lirbank <mikael@lirbank.com>
2026-02-24 07:55:45 -0800

    Update schema.ts

--- examples/nextjs-otp/app/schema.ts
+++ examples/nextjs-otp/app/schema.ts
@@ -1,9 +1,15 @@
 import { z } from "zod";
 
+/**
+ * Request OTP schema
+ */
 export const requestOtpSchema = z.object({
   identifier: z.email(),
 });
 
+/**
+ * Verify OTP schema
+ */
 export const verifyOtpSchema = z.object({
   identifier: z.email(),
   otp: z.string().length(6),
