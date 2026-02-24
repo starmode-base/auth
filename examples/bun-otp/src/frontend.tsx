c615932ea669799016073044887fdf98318912ad
Mikael Lirbank <mikael@lirbank.com>
2026-02-24 06:35:25 -0800

    mv

--- examples/bun-otp/src/frontend.tsx
+++ examples/bun-otp/src/frontend.tsx
@@ -6,7 +6,7 @@
  */
 
 import { createRoot } from "react-dom/client";
-import { App } from "./_app";
+import { App } from "./app";
 
 function start() {
   const root = createRoot(document.getElementById("root")!);
